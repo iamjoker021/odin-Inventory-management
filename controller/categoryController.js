6307cd30779bfc4678d549e80fb492d3c8bfd6ee
iamjoker021 <iamjoker021@gmail.com>
2024-08-02 10:39:38 +0530

    feat: Add Category Controller

---
+++ controller/categoryController.js
@@ -0,0 +1,31 @@
+const categoryModel = require('../model/category')
+
+const getAllCategory = async (req, res) => {
+    const categories = await categoryModel.getAllCategory();
+    res.send(categories);
+}
+
+const createCategory = async (req, res) => {
+    const { 'categroy-name': newCategory } = req.body;
+    await categoryModel.addNewCategory(newCategory);
+    res.redirect('/');
+}
+
+const editCategory = async (req, res) => {
+    const { 'categroy-name': newCategroyName } = req.body;
+    await categoryModel.updateCategroy(newCategroyName);
+    res.redirect('/');
+}
+
+const deleteCategory = async (req, res) => {
+    const { 'categroy-id': categriyId } = req.body;
+    await categoryModel.deleteCategory(categriyId);
+    res.redirect('/');
+}
+
+module.exports = {
+    getAllCategory,
+    createCategory,
+    editCategory,
+    deleteCategory
+}
